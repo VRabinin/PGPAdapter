c8630266172a039dcbc566c0d12a1e50797e7cf3
VRabinin <valery.ryabinin@gmail.com>
2015-12-21 13:37:10 +0100

    pgp example

---
+++ pgp_example/PGPTest.java
@@ -0,0 +1,32 @@
+package testpgp;
+
+import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+import java.io.ByteArrayOutputStream;
+
+public class PGPTest {
+	public static void main(String[] args) {
+		FileInputStream fin = null;
+		ByteArrayOutputStream out = null;;
+	    AbstractTrace trace = null;
+	    
+		System.out.println("Start!!!");
+		PGPCrypto crypto = new PGPCrypto();	
+		try{
+		fin = new FileInputStream("./resources/test.csv.pgp"); 
+		}
+		catch (FileNotFoundException e){
+			e.printStackTrace();
+		}
+		out = new ByteArrayOutputStream();
+		try{
+		crypto.decrypt(fin, out, "Qwerty123", trace);
+		}
+		 catch (Exception e) {
+			 e.printStackTrace();
+		 }
+		System.out.println(out.toString());
+
+	}
+
+}
